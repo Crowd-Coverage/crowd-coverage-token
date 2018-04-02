44d7b0d7f2127efdac4f4feb235c151a32442a7f
pllearns <pllearns@gmail.com>
2018-04-02 12:18:58 -0700

    add test, not parsing correctly

---
+++ test/COVRToken.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const compiledToken = require('../build/contracts/COVRToken.json');
+
+let accounts;
+let tokenAddress;
+let token;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  console.log(accounts);
+
+  token = await new web3.eth.Contract(JSON.parse(compiledToken.abi))
+    .deploy({ data: compiledToken.bytecode })
+  console.log('do I get the data? ==> ', data);
+});
+
+
+describe('COVR Token', () => {
+  it('mints a COVR token for use in Crowd Coverage', () => {
+    console.log('first test');
+  });
+});
